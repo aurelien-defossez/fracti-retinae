93b5177889dfed55970433a6bca45f819d58b962
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-12 20:51:09 +0200

    🎭 Lever model imported.

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -9,15 +9,38 @@ namespace FractiRetinae
 	public class Switch : MonoBehaviour
 	{
 		[SerializeField] private SlidingPlatform[] platforms;
+		[SerializeField, Min(0)] private float activationRotation;
+		[SerializeField] private Transform lever;
+		[SerializeField] private EaseDefinition animationEase;
+
+		private bool isLeft;
+		private Coroutine switchRoutine;
+
+		protected void OnEnable()
+		{
+			isLeft = true;
+			lever.transform.localRotation = Quaternion.Euler(0, 0, -activationRotation);
+		}
 
 		public void Activate()
 		{
 			Debug.Log($"Activate {name}");
 
+			this.RestartCoroutine(ref switchRoutine, Rotate());
+
 			foreach (SlidingPlatform platform in platforms)
 			{
 				platform.Switch();
 			}
+		}
+
+		private IEnumerator Rotate()
+		{
+			float to = isLeft ? activationRotation : -activationRotation;
+			Debug.Log($"Rotate {name} to {to}, isLeft={isLeft}");
+			isLeft = !isLeft;
+
+			yield return lever.transform.RotateTo(Quaternion.Euler(0, 0, to), animationEase);
 		}
 	}
 }
