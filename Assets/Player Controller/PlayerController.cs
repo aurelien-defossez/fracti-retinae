ab5c6fbda9ac74efeadf4c98f14a6701997b1f77
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-12 09:18:44 +0200

    ✨ Horizontal look added on player controller.

--- Assets/Player Controller/PlayerController.cs
+++ Assets/Player Controller/PlayerController.cs
@@ -9,22 +9,34 @@ namespace FractiRetinae
 {
 	public class PlayerController : MonoBehaviour
 	{
-		[SerializeField, Min(0)] private float walkSpeed;
+		[SerializeField, Min(0)] private float walkSpeed = 1;
+		[SerializeField, Min(0)] private float xSensitivity = 1;
+		[SerializeField, Min(0)] private float ySensitivity = 1;
 
 		private PlayerControls controls;
-		private Vector2 movementDirection;
 
 		protected void Awake()
 		{
 			controls = new PlayerControls();
+		}
+
+		protected void Start()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
 
 			controls.Player.Enable();
 		}
 
 		protected void Update()
 		{
-			movementDirection = controls.Player.Move.ReadValue<Vector2>();
-			transform.position = transform.position + new Vector3(movementDirection.x, 0, movementDirection.y) * Time.deltaTime * walkSpeed;
+			// Look
+			Vector2 lookDirection = controls.Player.Look.ReadValue<Vector2>();
+			transform.Rotate(Vector3.up, lookDirection.x * xSensitivity * Time.deltaTime);
+
+			// Movement
+			Vector2 movementDirection = controls.Player.Move.ReadValue<Vector2>();
+			transform.position = transform.position + new Vector3(movementDirection.x, 0, movementDirection.y) * walkSpeed * Time.deltaTime;
 		}
 	}
 }
