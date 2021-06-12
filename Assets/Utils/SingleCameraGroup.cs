e6e1abf3382d405068bd05aedb29a27f2b85b7a7
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-12 17:02:04 +0200

    ♻ SingleCameraGroup class created.

---
+++ Assets/Utils/SingleCameraGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SingleCameraGroup : MonoBehaviour
+{
+	[SerializeField, Range(1, 4)] int cameraIndex = 1;
+
+	protected void Awake()
+	{
+		int cameraLayer = LayerMask.NameToLayer("Camera" + cameraIndex);
+
+		foreach (Transform child in GetComponentsInChildren<Transform>(includeInactive: true))
+		{
+			child.gameObject.layer = cameraLayer;
+		}
+	}
+}
