59400c25b21f84808ec8f83f3425c9bbfc1f1dcf
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-13 08:15:19 +0200

    🔊 Volume added on music manager.

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,41 +2,42 @@ using UnityEngine;
 
 namespace FractiRetinae
 {
-    public class MusicManager : MonoBehaviour
-    {
-        public AudioSource explorationSource;
-        public AudioSource glyphHuntSource;
+	public class MusicManager : MonoBehaviour
+	{
+		[SerializeField, Range(0, 1)] private float volume;
+		[SerializeField] private AudioSource explorationSource;
+		[SerializeField] private AudioSource glyphHuntSource;
 
-        public void OnLevelStart()
-        {
-            if (Cheater.Instance.MuteMusic)
-            {
-                MuteMusic();
-            }
-            else
-            {
-                explorationSource.volume = 1.0f;
-                glyphHuntSource.volume = 0.0f;
-            }
-        }
+		public void OnLevelStart()
+		{
+			if (Cheater.Instance.MuteMusic)
+			{
+				MuteMusic();
+			}
+			else
+			{
+				explorationSource.volume = volume;
+				glyphHuntSource.volume = 0.0f;
+			}
+		}
 
-        public void OnGoalFound()
-        {
-            if (Cheater.Instance.MuteMusic)
-            {
-                MuteMusic();
-            }
-            else
-            {
-                explorationSource.volume = 0.0f;
-                glyphHuntSource.volume = 1.0f;
-            }
-        }
+		public void OnGoalFound()
+		{
+			if (Cheater.Instance.MuteMusic)
+			{
+				MuteMusic();
+			}
+			else
+			{
+				explorationSource.volume = 0.0f;
+				glyphHuntSource.volume = volume;
+			}
+		}
 
-        public void MuteMusic()
-        {
-            explorationSource.volume = 0.0f;
-            glyphHuntSource.volume = 0.0f;
-        }
-    }
+		public void MuteMusic()
+		{
+			explorationSource.volume = 0.0f;
+			glyphHuntSource.volume = 0.0f;
+		}
+	}
 }
