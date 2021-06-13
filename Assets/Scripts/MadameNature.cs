177202b48dc3e6b223c103f94a1cd4d5dfeef713
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-13 15:09:10 +0200

    🎭 Skybox and fog changes when touching the goal.

--- Assets/Scripts/MadameNature.cs
+++ Assets/Scripts/MadameNature.cs
@@ -9,17 +9,23 @@ namespace FractiRetinae
 	public class MadameNature : MonoBehaviourSingleton<MadameNature>
 	{
 		[SerializeField] private GameObject ambiance1Land, ambiance2Land;
+		[SerializeField] private Material ambiance1Skybox, ambiance2Skybox;
+		[SerializeField] private Color ambiance1FogColor, ambiance2FogColor;
 
 		public void OnLevelStart()
 		{
 			ambiance1Land.SetActive(true);
 			ambiance2Land.SetActive(false);
+			RenderSettings.skybox = ambiance1Skybox;
+			RenderSettings.fogColor = ambiance1FogColor;
 		}
 
 		public void OnGoalFound()
 		{
 			ambiance1Land.SetActive(false);
 			ambiance2Land.SetActive(true);
+			RenderSettings.skybox = ambiance2Skybox;
+			RenderSettings.fogColor = ambiance2FogColor;
 		}
 	}
 }
