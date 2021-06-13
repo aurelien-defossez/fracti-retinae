cbd4e14582a7ec57ca433622ee88b73892543b89
Aurélien Defossez <defossez.aurelien@gmail.com>
2021-06-13 19:39:03 +0200

    ✨ Title scene added.

---
+++ Assets/Scripts/Title/TitleScene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FractiRetinae.Title
+{
+	public class TitleScene : MonoBehaviour
+	{
+		public void PlayGame() => SceneManager.LoadScene("Game");
+
+		public void QuitGame()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+		}
+	}
+}
